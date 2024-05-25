92a1c7c53e5bea9e00537ea2ba9908b81f25e75b
Bapu <1.bapu.ojha@gmail.com>
2024-05-25 11:24:05 +0530

    Notifications page api fix

--- src/pages/Notifications.jsx
+++ src/pages/Notifications.jsx
@@ -1,15 +1,53 @@
-import { useState } from "react"
+import { useContext, useEffect, useState } from "react"
+import axios from 'axios'
+import { UserContext } from "../App";
+import { filterPaginationData } from "../common/FilterPagination";
 
 function Notifications() {
 
+    let { userAuth: { accessToken } } = useContext(UserContext)
+
     const [filter, setFilter] = useState('all');
+    const [notifications, setNotifications] = useState(null)
 
     let filters = ['all', 'like', 'comment', 'reply'];
+
+
+    const fetchNotifications = ({ page, deletedDocCount = 0 }) => {
+        axios.post(import.meta.env.VITE_SERVER_DOMAIN + "/notifications", { page, filter, deletedDocCount }, {
+            headers: {
+                Authorization: `Bearer ${accessToken}`
+            }
+        })
+            .then(async ({ data: { notifications: data } }) => {
+
+                let formatedData = await filterPaginationData({
+                    state: notifications,
+                    data, page,
+                    countRoute: "/all-notifications-count",
+                    data_to_send: { filter },
+                    user : accessToken
+                })
+
+                setNotifications(formatedData)
+                console.log(formatedData);
+            })
+            .catch(err => console.log(err))
+    }
+
+    useEffect(() => {
+
+        if(accessToken){
+            fetchNotifications({page : 1})
+        }
+
+    }, [accessToken, filter])
 
     const handleSelectedClick = (e) => {
         let button = e.target;
 
         setFilter(button.innerText.toLowerCase());
+        setNotifications(null)
 
     }
 
